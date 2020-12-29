34fd6d4dd0f7e930d0b956ca60594c6508407cf2
shunak <shuna966@gmail.com>
2020-12-29 12:29:41 +0900

    added crate definitions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate pnet;
+
+use pnet::datalink;
+use pnet::datalink::Channel::Ethernet;
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::ipv6::Ipv6packet;
+use pnet::packet::tcp::TcpPacket;
+use pnet::packet::udp::UdpPacket;
+use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::packet::Packet;
+
+
+use std::env;
+
+mod packets;
+use packets::GettabelEndPoints;
+
+const WIDTH::usize = 20;
+
+
 fn main() {
     println!("Hello, world!");
 }
