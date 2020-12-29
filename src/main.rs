96e0a3355e719ec839d9625a53a10a26a55af432
shunak <shuna966@gmail.com>
2020-12-29 12:37:53 +0900

    implement network interfaces

--- src/main.rs
+++ src/main.rs
@@ -20,5 +20,20 @@ const WIDTH::usize = 20;
 
 
 fn main() {
-    println!("Hello, world!");
+    env::set_var("RUST_LOG", "debug");
+    env_loggger::init();
+    let args: Vec<String> = env::args().collect();
+    if args.len() !=2 {
+        error!("Please specify target interface name")
+        std::process::exit(1);
+    }
+    let interface_name = &args[1];
+
+    // select interface
+    let interfaces = datalink::interfaces();
+    let interface = interfaces
+        .into_iter()
+        .find(|iface| iface.name == *interface_name)
+        .expect("Failed to get interrface");
+
 }
