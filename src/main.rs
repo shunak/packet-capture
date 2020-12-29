01f846a2730b07a8cf8db543a0f9ee779720ae48
shunak <shuna966@gmail.com>
2020-12-29 16:36:48 +0900

    constract ethernet frame & ipv4 handler

--- src/main.rs
+++ src/main.rs
@@ -35,5 +35,59 @@ fn main() {
         .into_iter()
         .find(|iface| iface.name == *interface_name)
         .expect("Failed to get interrface");
-
+    
+    // get datalink channel
+    let (_tx, mut rx) = match datalink::channel(&interface, Default::default()){
+        Ok(Ethernet(tx,rx)=>(tx,rx),
+        Ok(_) => panic!("Unhandled channel tyep"),
+        Err(e) => panic!("Failed to create datalinl channel {}",e)),
+    };
+    loop {
+        match rx.next() {
+            Ok(frame) => {
+                // construct ethernet frame from received data
+                let frame = EthernetPacket::new(frame).unwrap();
+                match frame.get_ethertype(){
+                    EtherTypes::Ipv4 => {
+                        ipv4_handler(&frame);
+                    }
+                    EtherTypes::Ipv6 => {
+                        ipv6_handler(&frame);
+                    }
+                    _ => {
+                        info!("Not an IPv4 or IPv6 packet");
+                    }
+                }
+            }
+            Err(e)=>{
+                error!("Failed to read: {}",e);
+            }
+        }
+    }
 }
+
+
+/**
+ * Construct IPv4 packet and call handler
+*/
+fn ipv4_handler(ethernet: &EthernetPacket) {
+    if let Some(packet) = Ipv4Packet::new(ethernet.payload()){
+        match packet.get_next_level_protocol(){
+            IpNextHeaderProtocols::Tcp => {
+                tcp_handler(&packet);
+            }
+            IpNextHeaderProtocols::Udp => {
+                udp_handler(&packet);
+            }
+            _ => {
+                info!("Not a TCP or UDP packet");
+            }
+        }
+    }
+}
+
+
+
+
+
+
