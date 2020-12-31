50abc8d7737f601fa2616f7198ff6d720fb00d2d
shunak <shuna966@gmail.com>
2021-01-01 00:24:23 +0900

    implement method for Ipv4

--- src/packet.rs
+++ src/packet.rs
@@ -5,4 +5,23 @@ pub trait GettableEndPoints{
 }
 
 
+// implement method for Ipv4
+impl<'a> GettableEndPoints for Ipv4Packet<'a> {
+    fn get_source(&self) -> String {
+        self.get_source().to_string()
+    }
+
+    fn get_destination(&self) -> String {
+        self.get_destination().to_string()
+    }
+
+    fn get_payload(&self) -> &[u8]{
+        self.payload()
+    }
+}
+
+
+
+
+
 
